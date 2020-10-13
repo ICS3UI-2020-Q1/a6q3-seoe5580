dc46b5c5454513ce01c7018e9ace8f97990acf4f
seoe5580 <seoe5580@wrdsb.ca>
2020-10-13 01:56:42 +0000

    Actually done large number question

--- Main.java
+++ Main.java
@@ -16,19 +16,24 @@ public class Main {
     // Asks the user to input 10 integers
     System.out.println("Please enter in 10 integers to put into the array");
 
+    // sets up arrays
     int[] num = new int[10];
 
+    // gets 10 numbers from the user
     for(int i = 0; i < num.length; i++){
       num[i] = input.nextInt();
     }
 
-    int highestNum = num[0];
+    // declares a variable
+    int highestNum = 0;
 
+    // sets the value of highestNum to highest number in stored variable
     for(int i = 0; i < num.length; i++){
       if(num[i] > highestNum){
         highestNum = num[i];
       }
     }
+    // tells the user the highest number
     System.out.println("The largest number is " + highestNum);
   }
 }
